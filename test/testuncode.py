14f08ca5502c7b865aa377912c44f1d0ea905614
kerwin <lvyitao@qq.com>
2020-05-20 11:09:20 +0800

    2020.05.20.2

--- test/testuncode.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse
-g = "%E6%88%91%E4%BB%AC%E9%83%BD%E6%98%AF%E5%B0%8F%E8%AF%97%E4%BA%BA"
-a = urllib.parse.quote(g)
-# a = g.decode("utf-8")
-print(a)
-
-
-l = 1351 // 20
-print(l)
