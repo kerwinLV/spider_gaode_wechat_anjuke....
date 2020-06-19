db3017c66896965b676e1d4e4f3b9b7b9c7172aa
kerwin <2509456238@qq.com>
2020-06-19 18:33:26 +0800

    modify auto

--- fire/seleniumanqing.py
+++ fire/seleniumanqing.py
@@ -11,14 +11,19 @@ from lxml import etree
 from zhon.hanzi import punctuation
 # from requests.cookies import RequestsCookieJar
 import requests
-from tools.sqlconn import get_shanghaiyingjipool
+import os, sys
 
+path1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(path1)
+from tools.sqlconn import get_shanghaiyingjipool
 
 
 class COOKIE(object):
     url = 'https://passport.weibo.cn/signin/login?entry=mweibo&r=https%3A%2F%2Fweibo.cn%2F&backTitle=%CE%A2%B2%A9&vt='
+
     def __init__(self):
         self.browser = webdriver.Chrome()
+
     def getcookie(self):
         self.browser.get(self.url)
         self.browser.implicitly_wait(15)
@@ -39,60 +44,64 @@ class COOKIE(object):
         # jar = RequestsCookieJar()
         for cookie in cookies:
             if "name" in cookie.keys() and "value" in cookie.keys():
-                cookiestr = cookiestr + cookie["name"]+"="+cookie["value"]+"; "
+                cookiestr = cookiestr + cookie["name"] + "=" + cookie["value"] + "; "
         # print(cookiestr)
-                # jar.set(cookie["name"],cookie["value"])
-                # cookie_dic[cookie["name"].encode('utf-8')]=cookie["value"].encode('utf-8')
+        # jar.set(cookie["name"],cookie["value"])
+        # cookie_dic[cookie["name"].encode('utf-8')]=cookie["value"].encode('utf-8')
         # print(jar)
         # print(cookie_dic)
-
 
         return cookiestr
 
 
-def get_msnage(cookiestr,url1):
+def get_msnage(cookiestr, url1):
     headers = {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
-                "Cookie":cookiestr,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Cookie": cookiestr,
     }
-    re1 = requests.get(url1,headers=headers)
+    re1 = requests.get(url1, headers=headers)
     # re1.encoding = "utf-8"
-    etem = etree.HTML(re1.text.replace('<?xml version="1.0" encoding="UTF-8"?>',''))
+    etem = etree.HTML(re1.text.replace('<?xml version="1.0" encoding="UTF-8"?>', ''))
     context = etem.xpath('//div[starts-with(@id, "M_")]')
     # print(context)
     for con in context:
-        msg = con.xpath('string(./div/span[@class="ctt"])').replace(":","")
+        msg = con.xpath('string(./div/span[@class="ctt"])').replace(":", "")
         ct = con.xpath('string(./div/span[@class="ct"])')
         ct = ct.encode("gbk", "ignore").decode("gbk", "ignore")
+        if "前" in ct:
+            minute1 = ct.split("来自")[0].split("分钟前")[0]
+            d1 = datetime.datetime.now()
+            ct = (d1 - datetime.timedelta(minutes=minute1)).strftime("%Y-%m-%d %H:%M:%S")
+            # print(d3)
         ct = ct.split("来自")[0]
         print(ct)
         if "年" not in ct and "-" not in ct and "今" not in ct:
             year1 = datetime.datetime.now().year
-            ct = (str(year1)+"年"+ct).replace("年","-").replace("月","-").replace("日","")
+            ct = (str(year1) + "年" + ct).replace("年", "-").replace("月", "-").replace("日", "")
             # ct = ct.encode("gbk", "ignore").decode("gbk", "ignore")
             # print(ct)
         elif "今天" in ct:
             totime = datetime.date.today()
-            ct = ct.replace("今天",totime.strftime("%Y-%m-%d"))
+            ct = ct.replace("今天", totime.strftime("%Y-%m-%d"))
             # ct = ct.encode("gbk","ignore").decode("gbk","ignore")
             # print(ct)
         nikename = con.xpath('./div/a[1]/text()')[0]
         wherefrom = "新浪微博"
         ctlen = len(ct.split(":"))
-        if ctlen==2:
+        if ctlen == 2:
             ct = int(time.mktime(time.strptime(ct, '%Y-%m-%d %H:%M')))
         else:
             ct = int(time.mktime(time.strptime(ct, '%Y-%m-%d %H:%M:%S')))
         release_time = ct
         # msg = re.sub(r"[%s]+" % punctuation, "", msg.decode("utf-8"))
         # print(msg.encode("gbk","ignore").decode("gbk","ignore"))
-        storage_time =int(time.time())
+        storage_time = int(time.time())
         # print(ct)
         # print(nikename)
-        save_sql(msg,nikename,wherefrom,release_time,storage_time)
+        save_sql(msg, nikename, wherefrom, release_time, storage_time)
 
 
-def save_sql(text,img,nikename,wherefrom,t_time):
+def save_sql(text, img, nikename, wherefrom, t_time):
     conn = get_shanghaiyingjipool().connection()
     cur = conn.cursor()
     sql = 'select * from anqing_xiaofang_a where context=%s'
@@ -108,17 +117,32 @@ def save_sql(text,img,nikename,wherefrom,t_time):
     cur.close()
     conn.close()
 
-if __name__=="__main__":
+def get_sourecfrom():
+    conn = get_shanghaiyingjipool().connection()
+    cur = conn.cursor()
+    sql = 'select * from sourcefrom'
+    cur.execute(sql)
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
+
+if __name__ == "__main__":
+    print(datetime.datetime.now())
     co = COOKIE()
     # co.getcookie()
+    sourecfrom = get_sourecfrom()
+    # print(sourecfrom)
     cookiestr = co.getcookie()
 
-    url1 = "https://weibo.cn/search/mblog?hideSearchFrame=&keyword=%E7%81%AB%E7%81%BE&advancedfilter=1&nick=%E5%A4%AE%E8%A7%86%E6%96%B0%E9%97%BB&endtime=20200616&sort=time&page={}"
-    for i in range(1,90):
-        print(i)
-        url2 = url1.format(i)
-        print(url2)
-        get_msnage(cookiestr,url2)
-        time.sleep(2)
+    for sf in sourecfrom:
+        url1 = "https://weibo.cn/search/mblog?hideSearchFrame=&keyword=%E7%81%AB%E7%81%BE&advancedfilter=1&nick={}&endtime=20200616&sort=time&page={}"
+        for i in range(1, 3):
+            print(i)
+            url2 = url1.format(sf["nickname"],i)
+            print(url2)
+            get_msnage(cookiestr, url2)
+            time.sleep(3)
 
     # print(re1.text.encode("gbk","ignore").decode("gbk","ignore"))
